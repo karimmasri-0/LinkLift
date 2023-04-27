2a14f63df7430ecb41296d9a7e425b3e3b16a6ce
XKESAR <karimmasri52@gmail.com>
2023-04-27 14:17:29 +0300

    Remove all files and folders

--- routes/topReviewsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const topReviewsController = require("../controllers/topReviewsController");
-
-router.post("/add", (req, res) => {
-  topReviewsController.addReview(req, res);
-});
-
-module.exports = router;
